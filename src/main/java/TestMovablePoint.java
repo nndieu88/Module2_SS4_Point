3ff6af60bcb8baed9d67bc374b43621dc3935c8d
nndieu <nndieu88@gmail.com>
2019-09-12 17:00:58 +0700

    SS4

---
+++ src/main/java/TestMovablePoint.java
@@ -0,0 +1,9 @@
+public class TestMovablePoint{
+    public static void main(String[] args) {
+        System.out.println(new MovablePoint());
+        System.out.println(new MovablePoint(3.4f,6.7f));
+        System.out.println(new MovablePoint(1.0f,1.0f,5.0f,6.0f).move());
+
+
+    }
+}
