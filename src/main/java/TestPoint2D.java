ddd51049c3a0e1469412ae858481e727575b2212
nndieu <nndieu88@gmail.com>
2019-09-12 16:18:38 +0700

    SS4

---
+++ src/main/java/TestPoint2D.java
@@ -0,0 +1,6 @@
+public class TestPoint2D {
+    public static void main(String[] args) {
+        System.out.println(new Point2D());
+        System.out.println(new Point2D(5.6f, 7.8f));
+    }
+}
---
+++ src/main/java/TestPoint3D.java
@@ -0,0 +1,6 @@
+public class TestPoint3D {
+    public static void main(String[] args) {
+        System.out.println(new Point3D());
+        System.out.println(new Point3D(1.4f, 3.5f, 7.0f));
+    }
+}
